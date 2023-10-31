f9eb1658bde87b1dd3061de9bdd3a03c0fc36f27
Rimsha Naeem <rimshanaeem595@gmail.com>
2023-11-01 03:14:42 +0500

    s

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGOURL)
     });
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'travel-pins-frontend.vercel.app',
 };
 app.use(cors(corsOptions));
 
